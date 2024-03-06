389348234d203733352729751b58d8649f2227cf
Wesley <113936684+WesleyS08@users.noreply.github.com>
2024-03-06 01:06:25 +0000

    finalização

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+
+  green_100: "#B870FF",
+  green_600: "#680DC2",
+
+  yellow_500: "#EFB103",
+
+  gray_200: "#C4C4CC",
+  gray_300: "#8D8D99",
+  gray_400: "#7C7C8A",
+}
